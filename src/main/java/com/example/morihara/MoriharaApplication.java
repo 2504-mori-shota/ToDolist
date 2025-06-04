c0e4e4eb659704a6ce106f842fe08a0a014827e0
moir_shota <mori.shota.q@alhinc.jp>
2025-06-04 10:16:34 +0900

    First Commit

---
+++ src/main/java/com/example/morihara/MoriharaApplication.java
@@ -0,0 +1,13 @@
+package com.example.morihara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoriharaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoriharaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/morihara/MoriharaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.morihara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoriharaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
